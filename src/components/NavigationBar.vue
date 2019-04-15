1251d7b0f4d58e9425259d3ef40374e47df7a687
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-16 00:44:28 +0800

    Create navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar is-primary">
+    <div class="navbar-brand">
+      <a class="navbar-item is-size-5">
+        Vue-modoro
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <router-link
+          v-for="link in links"
+          :key="link.text"
+          active-class="has-text-weight-semibold"
+          class="navbar-item"
+          tag="a"
+          :to="link.to"
+        >
+          <a>{{ link.text }}</a>
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { text: "Home", to: { name: "home" } },
+        { text: "About", to: { name: "about" }}
+      ]
+    };
+  }
+};
+</script>
