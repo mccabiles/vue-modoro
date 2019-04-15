8d78bd748f93a5751e5f06af51da8503110cfd0e
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-16 00:36:45 +0800

    Create timer progress bar component

---
+++ src/components/TimerProgressBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <progress class="progress" :max="timeMax" :value="timeCurr"></progress>
+    <p class="has-text-centered"> {{ timeCurr | timestamp }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    timeMax: [Number, String],
+    timeCurr: [Number, String]
+  },
+
+  filters: {
+    timestamp (val) {
+      const minutes = Math.floor(val / 60000).toString().padStart(2, "0");
+      const seconds = (val / 1000 % 60).toString().padStart(2, "0");
+      return `${minutes}:${seconds}`;
+    }
+  }
+};
+</script>
