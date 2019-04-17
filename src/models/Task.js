2071c6a6792bb058f5f18efdbba5c889de0ab607
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-17 23:34:06 +0800

    Create Task model

---
+++ src/models/Task.js
@@ -0,0 +1,31 @@
+export default class Task {
+  constructor (title) {
+    this.title = title;
+    this.id = new Date().getTime();
+  }
+
+  static getAllTasks() {
+    const tasks = localStorage.getItem("tasks");
+    if (tasks === null) return {};
+    return JSON.parse(tasks);
+  }
+
+  static createTask(title) {
+    let tasks = Task.getAllTasks();
+    const newTask = new Task(title);
+    tasks[newTask.id] = newTask;
+    Task.saveTasks(tasks);
+    return newTask;
+  }
+
+  static saveTasks(tasks) {
+    const stringified = JSON.stringify(tasks);
+    localStorage.setItem("tasks", stringified);
+  }
+
+  delete() {
+    let tasks = Task.getAllTasks();
+    delete tasks[this.id];
+    Task.saveTasks(tasks);
+  }
+}
