499bca8d47f4c80a12173b8cf5dcade028a9de18
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-18 01:34:44 +0800

    Edit about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>About the Creator</h1>
+    <p>
+      This pomodoro timer was created by Miguel Cabiles {{ "<" }}mccabiles@up.edu.ph{{ ">" }}.
+    </p>
   </div>
 </template>
