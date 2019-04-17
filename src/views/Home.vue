989aa1dd2fbabafd6b1b580cf2d94529c2403463
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-17 23:49:07 +0800

    Add Task list to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,63 @@
 <template>
-  <body>
-    <timer></timer>
+  <body class="container">
+    <div class="columns">
+      <div class="column is-one-third">
+        <task-list
+          :tasks="tasks"
+          v-model="newTask" 
+          @on:save="createTask"
+          @on:delete="deleteTask"
+        />
+      </div>
+
+      <div class="column">
+        <router-view></router-view>
+      </div>
+    </div>
   </body>
 </template>
 
 <script>
-import Timer from "@/components/Timer";
+import Task from '@/models/Task';
+import TaskList from "@/components/TaskList";
 export default {
-  name: "home",
+  name: "Home",
   components: {
-    Timer
+    TaskList
   },
+
+  data() {
+    return {
+      newTask: "",
+      tasks: {},
+    };
+  },
+
+  mounted () {
+    this.getTasks();
+  },
+
+  methods: {
+    getTasks () {
+      this.tasks = Task.getAllTasks();
+    },
+
+    createTask() {
+      Task.createTask(this.newTask);
+      this.newTask = "";
+      this.getTasks();
+    },
+
+    deleteTask(task) {
+      Task.delete(task.id);
+      this.getTasks();
+    }
+  }
 };
 </script>
+
+<style scoped>
+.container {
+  margin-top: 16px;
+}
+</style>
