1f13c681cfc693f5013838ad7bc6a67f374a6f33
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-18 01:20:31 +0800

    Create timer types and time lengths

--- src/store.js
+++ src/store.js
@@ -11,21 +11,32 @@ const timestamp = val => {
   return `${minutes}:${seconds}`;
 };
 
+const TIME_WORK = 1000 * 60 * 0.1;
+const TIME_BREAK_SHORT = 1000 * 60 * 0.2;
+const TIME_BREAK_LONG = 1000 * 60 * 0.3;
 const DEFAULT_TITLE = "Vue-modoro";
+const TIMER_TYPE_WORK = 1;
+const TIMER_TYPE_BREAK_SHORT = 2;
+const TIMER_TYPE_BREAK_LONG = 3;
 
 export default new Vuex.Store({
   state: {
     activeTask: null,
-    round: 0,
+    round: 1,
     timerActive: false,
-    timerMax: 5000,
-    timerRemaining: 5000,
+    timerType: TIMER_TYPE_WORK,
+    timerMax: TIME_WORK,
+    timerRemaining: TIME_WORK,
     timer: null
   },
 
   mutations: {
     setActiveTask(state, task) {
       state.activeTask = task;
+    },
+
+    setTimerType(state, timerType) {
+      state.timerType = timerType;
     },
 
     setTimerMax(state, timerMax) {
@@ -43,15 +54,11 @@ export default new Vuex.Store({
       document.title = DEFAULT_TITLE;
     },
 
-    deductTime(state, duration) {
-      if (state.timerRemaining <= 0) {
-        clearInterval(state.timer);
-        state.timerActive = false;
-        state.timer = null;
-        document.title = DEFAULT_TITLE;
-        return;
-      }
+    setRound(state, round) {
+      state.round = round;
+    },
 
+    deductTime(state, duration) {
       state.timerRemaining -= duration;
       document.title = timestamp(state.timerRemaining);
     },
@@ -64,11 +71,48 @@ export default new Vuex.Store({
   },
 
   actions: {
-    toggleTimer({ state, commit }) {
+    startTimer({ commit, dispatch }) {
+      const timer = setInterval(() => dispatch("deductTime", 1000), 1000);
+      commit("startTimer", timer);
+    },
+
+    deductTime({ state, commit, dispatch }) {
+      commit("deductTime", 1000);
+      if (state.timerRemaining > 0) return;
+      commit("stopTimer");
+      if (state.timerType != TIMER_TYPE_WORK) return dispatch("startWorkTimer");
+      if (state.round % 4 != 0) return dispatch("startBreakTimerShort");
+      return dispatch("startBreakTimerLong");
+    },
+
+    toggleTimer({ state, commit, dispatch }) {
       if (state.timerRemaining <= 0) return;
       if (state.timerActive) return commit("stopTimer");
-      const timer = setInterval(() => commit("deductTime", 1000), 1000);
-      commit("startTimer", timer);
-    }
+      dispatch("startTimer");
+    },
+
+    resetTimer({ state, commit }) {
+      commit("stopTimer");
+      commit("setTimerRemaining", state.timerMax);
+    },
+
+    startWorkTimer({ state, commit }) {
+      commit("setTimerMax", TIME_WORK);
+      commit("setTimerRemaining", TIME_WORK);
+      commit("setTimerType", TIMER_TYPE_WORK);
+      commit("setRound", state.round + 1);
+    },
+
+    startBreakTimerShort({ commit }) {
+      commit("setTimerMax", TIME_BREAK_SHORT);
+      commit("setTimerRemaining", TIME_BREAK_SHORT);
+      commit("setTimerType", TIMER_TYPE_BREAK_SHORT);
+    },
+
+    startBreakTimerLong({ commit }) {
+      commit("setTimerMax", TIME_BREAK_LONG);
+      commit("setTimerRemaining", TIME_BREAK_LONG);
+      commit("setTimerType", TIMER_TYPE_BREAK_LONG);
+    },
   }
 });
