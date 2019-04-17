d9200e8bba6d7d690debc4941739646989c03e07
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-18 00:20:50 +0800

    Add timer actions to View

--- src/views/View.vue
+++ src/views/View.vue
@@ -2,15 +2,18 @@
   <div class="box">
     <h2 class="is-size-3 has-text-centered">{{ task.title }}</h2>
     <timer-progress-bar />
+    <timer-actions style="margin-top: 1.5rem;" />
   </div>
 </template>
 
 <script>
-import Task from '@/models/Task'
-import TimerProgressBar from '@/components/TimerProgressBar'
+import Task from '@/models/Task';
+import TimerProgressBar from '@/components/TimerProgressBar';
+import TimerActions from '@/components/TimerActions';
 export default {
   name: 'ViewTask',
   components: {
+    TimerActions,
     TimerProgressBar
   },
 
@@ -27,10 +30,25 @@ export default {
     }
   },
 
+  beforeRouteUpdate(to, from, next) {
+    this.confirmNavigation(next);
+  },
+
+  beforeRouteLeave(to, from, next) {
+    this.confirmNavigation(next);
+  },
+
   data() {
     return {
       task: new Task('')
     };
+  },
+
+  methods: {
+    async confirmNavigation(next){
+      if (!confirm('Are you sure you want to leave the task? Your progress will be lost!')) return;
+      next();
+    }
   }
 };
 </script>
