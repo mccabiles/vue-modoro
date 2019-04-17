4a57cdcf54387a437eef15a7456839bfe9b299ff
Miguel Cabiles <mccabiles@up.edu.ph>
2019-04-17 23:34:14 +0800

    Create task list

---
+++ src/components/TaskList.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="panel">
+    <div class="panel-block">
+      <input
+        class="input"
+        placeholder="Add a new task"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        @keydown.enter="$emit('on:save')"
+      />
+    </div>
+    <div v-if="Object.keys(tasks).length == 0" class="has-text-centered panel-block">
+      <p>No tasks yet.</p>
+    </div>
+    <template v-else>
+      <a class="panel-block" v-for="(task, id) in tasks" :key="id">
+        {{ task.title }}
+      </a>
+    </template>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  props: {
+    tasks: {
+      type: Object,
+      default: () => ({})
+    },
+    value: String
+  }
+};
+</script>
